8620bb452830895bff33631fc09ccf7877e430c5
Imaad Uddin <imaaduddin@icloud.com>
2021-07-05 20:01:28 -0500

    variables

---
+++ Day 1 Working With Variables/Day-1-Input-Function-Exercise.py
@@ -0,0 +1,7 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+name = input("What is your name? ")
+
+num_of_char = len(name)
+
+print(num_of_char)
