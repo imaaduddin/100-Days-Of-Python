4045d31f0bcfc9a8db4cad14301099312423cb75
Imaad Uddin <imaaduddin@icloud.com>
2021-07-06 23:52:18 -0500

    day 2 - data types

---
+++ Day 2 Understanding Data Types/Day-2-Math-Operator.py
@@ -0,0 +1,12 @@
+print(3 + 2)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3) # division always results in a float
+print(2 ** 3) # ** means to the power of
+
+
+# PEMDAS
+# ()
+# **
+# * /
+# + -
---
+++ Day 2 Understanding Data Types/Day-2-Your-Life-In-Weeks.py
@@ -0,0 +1,26 @@
+# I was reading this article by Tim Urban - Your Life in Weeks and realised just how little time we actually have.
+
+# https://waitbutwhy.com/2014/05/life-weeks.html
+
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+# It will take your current age as the input and output a message with our time left in this format:
+
+# You have x days, y weeks, and z months left.
+
+# Where x, y and z are replaced with the actual calculated numbers.
+
+# Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+current_age = int(age)
+
+max_age = 90
+
+difference_in_age = max_age - current_age
+
+print(f"You have {difference_in_age * 365} days, {difference_in_age * 52} weeks, and {difference_in_age * 12} months left.")
