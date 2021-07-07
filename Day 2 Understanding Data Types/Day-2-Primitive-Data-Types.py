9254a65befe35e714e811a36cd0cfe4a53e0ca6d
Imaad Uddin <imaaduddin@icloud.com>
2021-07-06 22:07:22 -0500

    data types bascs

---
+++ Day 2 Understanding Data Types/Day-2-Primitive-Data-Types.py
@@ -0,0 +1,15 @@
+# Data Types 
+
+# String
+print("Hello"[0]) # prints letter H since that is at index of 0
+
+# Integer 
+print(123)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
