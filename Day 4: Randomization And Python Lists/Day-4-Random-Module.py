943034cd5c19707b7c8f21e51bf497cd996bd2bb
Imaad Uddin <imaaduddin@icloud.com>
2021-07-25 17:45:44 -0500

    random module

---
+++ Day 4: Randomization And Python Lists/Day-4-Random-Module.py
@@ -0,0 +1,17 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+random_float * 5
+
+love_score = random.randint(1, 100)
+print(f"Your score is {love_score}")
+
+
+
+
+
