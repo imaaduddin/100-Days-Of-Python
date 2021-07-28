08ce0b291829ce244ced3a319167a805d9964392
Imaad Uddin <imaaduddin@icloud.com>
2021-07-27 22:32:21 -0500

    practicing

---
+++ Day 4: Randomization And Python Lists/Day-4-Lists.py
@@ -0,0 +1,6 @@
+states_of_america = ["New York", "Texas", "California", "Florida", "Arizona", "Nevada", "Washington"]
+
+print(states_of_america[0])
+
+states_of_america.append("Georgia")
+print(states_of_america)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Covering all of Python in 100 days.      
+Covering all of Python in 100 days.    
+
+A reference guide of Python for me.
