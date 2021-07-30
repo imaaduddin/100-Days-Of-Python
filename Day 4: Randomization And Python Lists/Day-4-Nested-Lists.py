ca12acd8c0e8bcb043886fc83a6fce485b18bd99
Imaad Uddin <imaaduddin@icloud.com>
2021-07-29 23:14:45 -0500

    nested lists

---
+++ Day 4: Randomization And Python Lists/Day-4-Nested-Lists.py
@@ -0,0 +1,8 @@
+import random
+
+
+states_of_america = ["New York", "Texas", "California", "Florida", "Arizona", "Nevada", "Washington"]
+
+len_of_states = len(states_of_america)
+
+print(states_of_america[len_of_states - 1])
