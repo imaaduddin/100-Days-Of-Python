0b564ce69b4ba121335089e0e338e7c801334d93
Imaad Uddin <imaaduddin@icloud.com>
2021-08-09 15:03:48 -0500

    exercise for nested lists

--- Day 4: Randomization And Python Lists/Day-4-Nested-Lists.py
+++ Day 4: Randomization And Python Lists/Day-4-Nested-Lists.py
@@ -6,3 +6,9 @@ states_of_america = ["New York", "Texas", "California", "Florida", "Arizona", "N
 len_of_states = len(states_of_america)
 
 print(states_of_america[len_of_states - 1])
+
+fruits = ["cherry", "apple", "grapes", "mango"]
+veggies = ["spinach", "carrots", "onions", "peas"]
+
+fruits_and_veggies = [fruits, veggies]
+print(fruits_and_veggies)
