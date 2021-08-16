c3a0d03736c81b9f17b453b01010e2d99e298f82
Imaad Uddin <imaaduddin@icloud.com>
2021-08-16 14:54:35 -0500

    rock paper and scissors

--- Day 4: Randomization And Python Lists/Day-4-Project-Rock-Paper-Scissors.py
+++ Day 4: Randomization And Python Lists/Day-4-Project-Rock-Paper-Scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,37 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+choices = [rock, paper, scissors]
+
+computer_choice = random.choice(choices)
+
+human_choice = random.choice(choices)
+
+if human_choice == paper and computer_choice == rock:
+    print(human_choice)
+    print("Humans win!")
+elif computer_choice == paper and human_choice == rock:
+    print(computer_choice)
+    print("Computer wins!")
+if human_choice == rock and computer_choice == scissors:
+    print(human_choice)
+    print("Go humans!")
+elif computer_choice == rock and human_choice == scissors:
+    print(computer_choice)
+    print("Computer wins...")
+if human_choice == scissors and computer_choice == paper:
+    print(human_choice)
+    print("Let's gooo humans!!!")
+elif computer_choice == scissors and human_choice == paper:
+    print(computer_choice)
+    print("Them robots win...")
+
+
+
+
+
+
+
+
+
