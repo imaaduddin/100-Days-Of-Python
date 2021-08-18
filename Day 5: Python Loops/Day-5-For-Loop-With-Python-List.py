31d77674616c969d56909c0af2a829d749cd9d38
Imaad Uddin <imaaduddin@icloud.com>
2021-08-18 12:45:17 -0500

    average height

---
+++ Day 5: Python Loops/Day-5-For-Loop-With-Python-List.py
@@ -0,0 +1,8 @@
+# For loop example
+# for item in list_of_items:
+  # Do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+  print(fruit) # executing this print statement 3 times 
+  print(fruit + " pie")
