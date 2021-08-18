7ddc06e5735b61f7b5d504ed9707ca784d56d5fa
Imaad Uddin <imaaduddin@icloud.com>
2021-08-18 12:04:05 -0500

    rock paper scissors project

--- Day 4: Randomization And Python Lists/Day-4-Project-Rock-Paper-Scissors.py
+++ Day 4: Randomization And Python Lists/Day-4-Project-Rock-Paper-Scissors.py
@@ -56,15 +56,18 @@ scissors = '''
 # Professor solution:
 game_images = [rock, paper, scissors]
 user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_input])
+
+if user_input >= 3 or user_input < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(game_images[user_input])
 
 computer_input = random.randint(0, 2)
 print("Computer chose:")
 print(game_images[computer_input])
 
-if user_input >= 3 or user_input < 0:
-    print("You typed an invalid number, you lose!")
-elif user_input == 0 and computer_input == 2:
+
+if user_input == 0 and computer_input == 2:
     print("User Wins!")
 elif computer_input == 0 and user_input == 2:
     print("User loses!")
